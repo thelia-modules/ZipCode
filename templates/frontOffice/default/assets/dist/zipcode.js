bc8fc50c89d2d82bd8a46fec3596f22def4361cb
Vincent Lopes <vlopes@openstudio.fr>
2017-08-31 16:31:08 +0200

    Fix on page without zipcode inputs

--- templates/frontOffice/default/assets/dist/zipcode.js
+++ templates/frontOffice/default/assets/dist/zipcode.js
@@ -1,130 +1,133 @@
 "use strict";
 
 var zipcodeInput = document.getElementById("zipcode");
-var zipcodeSuggestion = document.createElement("ul");
-zipcodeSuggestion.id = "zipcode_suggestion";
-zipcodeSuggestion.className = "hidden suggestion";
-zipcodeInput.parentNode.insertBefore(zipcodeSuggestion, zipcodeInput.nextSibling);
-zipcodeInput.addEventListener('change', function () {
-    geonamesCall(zipcodeInput.value, 'postalcode');
-});
-zipcodeInput.addEventListener('focus', function () {
-    zipcodeSuggestion.classList.remove("hidden");
-});
-zipcodeInput.addEventListener('blur', function () {
-    setTimeout(function () {
-        zipcodeSuggestion.classList.add("hidden");
-    }, 400);
-});
 
-var cityInput = document.getElementById("city");
-var citySuggestion = document.createElement("ul");
-citySuggestion.id = "city_suggestion";
-citySuggestion.className = "hidden suggestion";
-cityInput.parentNode.insertBefore(citySuggestion, cityInput.nextSibling);
-cityInput.addEventListener('change', function () {
-    geonamesCall(cityInput.value, 'placename');
-});
-cityInput.addEventListener('focus', function () {
+if (null !== zipcodeInput) {
+    var geonamesCall = function geonamesCall(search, searchType) {
+        clearSuggestion();
+        citySuggestion.append(loader.cloneNode(true));
+        zipcodeSuggestion.append(loader.cloneNode(true));
+        getIsoAlpha2(countrySelector.options[countrySelector.selectedIndex].value).then(function () {
+            var baseUrl = 'https://secure.geonames.org/postalCodeSearchJSON?maxRows=5&username=' + geonames_username + '&country=';
+            return fetch(baseUrl + countryIsoAlpha2 + "&" + searchType + '=' + search).then(function (response) {
+                return response.json();
+            }).then(function (response) {
+                showResults(response, searchType);
+            }).then(function (response) {
+                clearLoader();
+            });
+        });
+    };
 
-    citySuggestion.classList.remove("hidden");
-});
+    var showResults = function showResults(results, searchType) {
+        if (searchType === "postalcode") {
+            showCities(results);
+        } else {
+            showZipcodes(results);
+        }
+    };
 
-cityInput.addEventListener('blur', function () {
-    setTimeout(function () {
-        citySuggestion.classList.add("hidden");
-    }, 300);
-});
+    var showCities = function showCities(results) {
+        var postalCodeArray = results.postalCodes;
+        postalCodeArray.forEach(function (postalcode) {
+            var suggestion = createSuggestion('city');
+            suggestion.append(postalcode.placeName);
+            citySuggestion.append(suggestion);
+        });
+    };
 
-var countrySelector = document.getElementById("country");
-var countryIsoAlpha2 = 'fr';
+    var showZipcodes = function showZipcodes(results) {
+        var postalCodeArray = results.postalCodes;
+        postalCodeArray.forEach(function (postalcode) {
+            var suggestion = createSuggestion('zipcode');
+            suggestion.append(postalcode.postalCode);
+            zipcodeSuggestion.append(suggestion);
+        });
+    };
 
-var loader = document.createElement("li");
-loader.className = "loader";
-loader.append(createCircle('left'));
-loader.append(createCircle('middle'));
-loader.append(createCircle('right'));
+    var createSuggestion = function createSuggestion(suggestion) {
+        var li = document.createElement("li");
+        li.addEventListener('click', function () {
+            window[suggestion + 'Input'].value = this.innerHTML;
+            window[suggestion + 'Suggestion'].classList.add('hidden');
+            window[suggestion + 'Suggestion'].innerHTML = "";
+        });
+        return li;
+    };
 
-function geonamesCall(search, searchType) {
-    clearSuggestion();
-    citySuggestion.append(loader.cloneNode(true));
-    zipcodeSuggestion.append(loader.cloneNode(true));
-    getIsoAlpha2(countrySelector.options[countrySelector.selectedIndex].value).then(function () {
-        var baseUrl = 'https://secure.geonames.org/postalCodeSearchJSON?maxRows=5&username=' + geonames_username + '&country=';
-        return fetch(baseUrl + countryIsoAlpha2 + "&" + searchType + '=' + search).then(function (response) {
+    var clearSuggestion = function clearSuggestion() {
+        zipcodeSuggestion.classList.add('hidden');
+        zipcodeSuggestion.innerHTML = "";
+
+        citySuggestion.classList.add('hidden');
+        citySuggestion.innerHTML = "";
+    };
+
+    var createCircle = function createCircle(position) {
+        var circle = document.createElement("div");
+        circle.className = "circle " + position;
+        return circle;
+    };
+
+    var clearLoader = function clearLoader() {
+        var loaders = document.getElementsByClassName('loader');
+
+        while (loaders.length > 0) {
+            loaders[0].remove();
+        }
+    };
+
+    var getIsoAlpha2 = function getIsoAlpha2(id) {
+        return fetch("/zipcode/isoalpha2/" + id).then(function (response) {
             return response.json();
         }).then(function (response) {
-            showResults(response, searchType);
-        }).then(function (response) {
-            clearLoader();
+            countryIsoAlpha2 = response.isoalpha2;
+        }).catch(function (response) {
+            countryIsoAlpha2 = 'fr';
         });
+    };
+
+    var zipcodeSuggestion = document.createElement("ul");
+    zipcodeSuggestion.id = "zipcode_suggestion";
+    zipcodeSuggestion.className = "hidden suggestion";
+    zipcodeInput.parentNode.insertBefore(zipcodeSuggestion, zipcodeInput.nextSibling);
+    zipcodeInput.addEventListener('change', function () {
+        geonamesCall(zipcodeInput.value, 'postalcode');
     });
-}
-
-function showResults(results, searchType) {
-    if (searchType === "postalcode") {
-        showCities(results);
-    } else {
-        showZipcodes(results);
-    }
-}
-
-function showCities(results) {
-    var postalCodeArray = results.postalCodes;
-    postalCodeArray.forEach(function (postalcode) {
-        var suggestion = createSuggestion('city');
-        suggestion.append(postalcode.placeName);
-        citySuggestion.append(suggestion);
+    zipcodeInput.addEventListener('focus', function () {
+        zipcodeSuggestion.classList.remove("hidden");
     });
-}
-
-function showZipcodes(results) {
-    var postalCodeArray = results.postalCodes;
-    postalCodeArray.forEach(function (postalcode) {
-        var suggestion = createSuggestion('zipcode');
-        suggestion.append(postalcode.postalCode);
-        zipcodeSuggestion.append(suggestion);
+    zipcodeInput.addEventListener('blur', function () {
+        setTimeout(function () {
+            zipcodeSuggestion.classList.add("hidden");
+        }, 400);
     });
-}
 
-function createSuggestion(suggestion) {
-    var li = document.createElement("li");
-    li.addEventListener('click', function () {
-        window[suggestion + 'Input'].value = this.innerHTML;
-        window[suggestion + 'Suggestion'].classList.add('hidden');
-        window[suggestion + 'Suggestion'].innerHTML = "";
+    var cityInput = document.getElementById("city");
+    var citySuggestion = document.createElement("ul");
+    citySuggestion.id = "city_suggestion";
+    citySuggestion.className = "hidden suggestion";
+    cityInput.parentNode.insertBefore(citySuggestion, cityInput.nextSibling);
+    cityInput.addEventListener('change', function () {
+        geonamesCall(cityInput.value, 'placename');
     });
-    return li;
-}
+    cityInput.addEventListener('focus', function () {
 
-function clearSuggestion() {
-    zipcodeSuggestion.classList.add('hidden');
-    zipcodeSuggestion.innerHTML = "";
-
-    citySuggestion.classList.add('hidden');
-    citySuggestion.innerHTML = "";
-}
-
-function createCircle(position) {
-    var circle = document.createElement("div");
-    circle.className = "circle " + position;
-    return circle;
-}
-
-function clearLoader() {
-    var loaders = document.getElementsByClassName('loader');
-
-    while (loaders.length > 0) {
-        loaders[0].remove();
-    }
-}
-
-function getIsoAlpha2(id) {
-    return fetch("/zipcode/isoalpha2/" + id).then(function (response) {
-        return response.json();
-    }).then(function (response) {
-        countryIsoAlpha2 = response.isoalpha2;
-    }).catch(function (response) {
-        countryIsoAlpha2 = 'fr';
+        citySuggestion.classList.remove("hidden");
     });
+
+    cityInput.addEventListener('blur', function () {
+        setTimeout(function () {
+            citySuggestion.classList.add("hidden");
+        }, 300);
+    });
+
+    var countrySelector = document.getElementById("country");
+    var countryIsoAlpha2 = 'fr';
+
+    var loader = document.createElement("li");
+    loader.className = "loader";
+    loader.append(createCircle('left'));
+    loader.append(createCircle('middle'));
+    loader.append(createCircle('right'));
 }
--- templates/frontOffice/default/assets/src/zipcode.js
+++ templates/frontOffice/default/assets/src/zipcode.js
@@ -1,129 +1,132 @@
 let zipcodeInput = document.getElementById("zipcode");
-let zipcodeSuggestion = document.createElement("ul");
-zipcodeSuggestion.id = "zipcode_suggestion";
-zipcodeSuggestion.className = "hidden suggestion";
-zipcodeInput.parentNode.insertBefore(zipcodeSuggestion, zipcodeInput.nextSibling);
-zipcodeInput.addEventListener('change', function() {geonamesCall(zipcodeInput.value,'postalcode')});
-zipcodeInput.addEventListener('focus', function()
-{
-        zipcodeSuggestion.classList.remove("hidden")
-});
-zipcodeInput.addEventListener('blur', function()
-{
-    setTimeout(function() {
-        zipcodeSuggestion.classList.add("hidden")
-    }, 400)
-});
 
-let cityInput = document.getElementById("city");
-let citySuggestion = document.createElement("ul");
-citySuggestion.id = "city_suggestion";
-citySuggestion.className = "hidden suggestion";
-cityInput.parentNode.insertBefore(citySuggestion, cityInput.nextSibling);
-cityInput.addEventListener('change', function() {geonamesCall(cityInput.value,'placename')});
-cityInput.addEventListener('focus', function()
-{
+if (null !== zipcodeInput) {
+    let zipcodeSuggestion = document.createElement("ul");
+    zipcodeSuggestion.id = "zipcode_suggestion";
+    zipcodeSuggestion.className = "hidden suggestion";
+    zipcodeInput.parentNode.insertBefore(zipcodeSuggestion, zipcodeInput.nextSibling);
+    zipcodeInput.addEventListener('change', function() {geonamesCall(zipcodeInput.value,'postalcode')});
+    zipcodeInput.addEventListener('focus', function()
+    {
+        zipcodeSuggestion.classList.remove("hidden")
+    });
+    zipcodeInput.addEventListener('blur', function()
+    {
+        setTimeout(function() {
+            zipcodeSuggestion.classList.add("hidden")
+        }, 400)
+    });
+
+    let cityInput = document.getElementById("city");
+    let citySuggestion = document.createElement("ul");
+    citySuggestion.id = "city_suggestion";
+    citySuggestion.className = "hidden suggestion";
+    cityInput.parentNode.insertBefore(citySuggestion, cityInput.nextSibling);
+    cityInput.addEventListener('change', function() {geonamesCall(cityInput.value,'placename')});
+    cityInput.addEventListener('focus', function()
+    {
 
         citySuggestion.classList.remove("hidden")
-});
+    });
 
-cityInput.addEventListener('blur', function()
-{
-    setTimeout(function() {
-        citySuggestion.classList.add("hidden")
-    }, 300)
-});
+    cityInput.addEventListener('blur', function()
+    {
+        setTimeout(function() {
+            citySuggestion.classList.add("hidden")
+        }, 300)
+    });
 
-let countrySelector = document.getElementById("country");
-let countryIsoAlpha2 = 'fr';
+    let countrySelector = document.getElementById("country");
+    let countryIsoAlpha2 = 'fr';
 
-let loader = document.createElement("li");
-loader.className = "loader";
-loader.append(createCircle('left'));
-loader.append(createCircle('middle'));
-loader.append(createCircle('right'));
+    let loader = document.createElement("li");
+    loader.className = "loader";
+    loader.append(createCircle('left'));
+    loader.append(createCircle('middle'));
+    loader.append(createCircle('right'));
 
-function geonamesCall(search, searchType) {
-    clearSuggestion();
-    citySuggestion.append(loader.cloneNode(true));
-    zipcodeSuggestion.append(loader.cloneNode(true));
-    getIsoAlpha2(countrySelector.options[countrySelector.selectedIndex].value)
-        .then(function () {
-            let baseUrl = 'https://secure.geonames.org/postalCodeSearchJSON?maxRows=5&username='+geonames_username+'&country=';
-            return fetch(baseUrl+countryIsoAlpha2+"&"+searchType+'='+search)
-                .then(response => {
-                    return response.json();
-                }).then(response => {
-                    showResults(response, searchType)
-                }).then(response => {
-                    clearLoader()
-                });
-        }
-    )
-}
-
-function showResults(results, searchType) {
-    if (searchType === "postalcode") {
-        showCities(results);
-    } else {
-        showZipcodes(results);
+    function geonamesCall(search, searchType) {
+        clearSuggestion();
+        citySuggestion.append(loader.cloneNode(true));
+        zipcodeSuggestion.append(loader.cloneNode(true));
+        getIsoAlpha2(countrySelector.options[countrySelector.selectedIndex].value)
+            .then(function () {
+                    let baseUrl = 'https://secure.geonames.org/postalCodeSearchJSON?maxRows=5&username='+geonames_username+'&country=';
+                    return fetch(baseUrl+countryIsoAlpha2+"&"+searchType+'='+search)
+                        .then(response => {
+                            return response.json();
+                        }).then(response => {
+                            showResults(response, searchType)
+                        }).then(response => {
+                            clearLoader()
+                        });
+                }
+            )
     }
-}
 
-function showCities(results) {
-    let postalCodeArray = results.postalCodes;
-    postalCodeArray.forEach(postalcode => {
-        let suggestion = createSuggestion('city');
-        suggestion.append(postalcode.placeName);
-        citySuggestion.append(suggestion);
-    })
-}
+    function showResults(results, searchType) {
+        if (searchType === "postalcode") {
+            showCities(results);
+        } else {
+            showZipcodes(results);
+        }
+    }
 
-function showZipcodes(results) {
-    let postalCodeArray = results.postalCodes;
-    postalCodeArray.forEach(postalcode => {
-        let suggestion = createSuggestion('zipcode');
-        suggestion.append(postalcode.postalCode);
-        zipcodeSuggestion.append(suggestion);
-    })
-}
+    function showCities(results) {
+        let postalCodeArray = results.postalCodes;
+        postalCodeArray.forEach(postalcode => {
+            let suggestion = createSuggestion('city');
+            suggestion.append(postalcode.placeName);
+            citySuggestion.append(suggestion);
+        })
+    }
 
-function createSuggestion(suggestion) {
-    let li = document.createElement("li");
-    li.addEventListener('click', function() {
-        window[suggestion+'Input'].value = this.innerHTML;
-        window[suggestion+'Suggestion'].classList.add('hidden');
-        window[suggestion+'Suggestion'].innerHTML = "";
-    });
-    return li;
-}
+    function showZipcodes(results) {
+        let postalCodeArray = results.postalCodes;
+        postalCodeArray.forEach(postalcode => {
+            let suggestion = createSuggestion('zipcode');
+            suggestion.append(postalcode.postalCode);
+            zipcodeSuggestion.append(suggestion);
+        })
+    }
 
-function clearSuggestion() {
-    zipcodeSuggestion.classList.add('hidden');
-    zipcodeSuggestion.innerHTML = "";
+    function createSuggestion(suggestion) {
+        let li = document.createElement("li");
+        li.addEventListener('click', function() {
+            window[suggestion+'Input'].value = this.innerHTML;
+            window[suggestion+'Suggestion'].classList.add('hidden');
+            window[suggestion+'Suggestion'].innerHTML = "";
+        });
+        return li;
+    }
 
-    citySuggestion.classList.add('hidden');
-    citySuggestion.innerHTML = "";
-}
+    function clearSuggestion() {
+        zipcodeSuggestion.classList.add('hidden');
+        zipcodeSuggestion.innerHTML = "";
 
-function createCircle(position) {
-    let circle = document.createElement("div");
-    circle.className = "circle "+position;
-    return circle;
-}
+        citySuggestion.classList.add('hidden');
+        citySuggestion.innerHTML = "";
+    }
 
-function clearLoader() {
-    let loaders = document.getElementsByClassName('loader');
+    function createCircle(position) {
+        let circle = document.createElement("div");
+        circle.className = "circle "+position;
+        return circle;
+    }
 
-    while (loaders.length > 0) loaders[0].remove();
-}
+    function clearLoader() {
+        let loaders = document.getElementsByClassName('loader');
 
-function getIsoAlpha2(id) {
-    return fetch("/zipcode/isoalpha2/"+id).then(response => {
-        return response.json();
-    }).then(function(response) {
-        countryIsoAlpha2 = response.isoalpha2;
-    }).catch(function (response) {
-        countryIsoAlpha2 = 'fr';
-    });
+        while (loaders.length > 0) loaders[0].remove();
+    }
+
+    function getIsoAlpha2(id) {
+        return fetch("/zipcode/isoalpha2/"+id).then(response => {
+            return response.json();
+        }).then(function(response) {
+            countryIsoAlpha2 = response.isoalpha2;
+        }).catch(function (response) {
+            countryIsoAlpha2 = 'fr';
+        });
+    }
 }
