c0d1499bf2e2d82834108be6d5ffa37874e73eeb
Vincent Lopes <vlopes@openstudio.fr>
2017-10-05 17:10:49 +0200

    Add polyfill for fetch + uglify js

--- templates/frontOffice/default/gulpfile.js
+++ templates/frontOffice/default/gulpfile.js
@@ -2,11 +2,15 @@ var gulp = require("gulp");
 var babel = require("gulp-babel");
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task("babel", function () {
-    return gulp.src("assets/src/zipcode.js")
+    return gulp.src(["node_modules/whatwg-fetch/fetch.js", "assets/src/zipcode.js"])
         .pipe(babel())
-        .pipe(gulp.dest("assets/dist"));
+        .pipe(concat('zipcode.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest("assets/dist/"));
 });
 
 gulp.task("scss", function () {
